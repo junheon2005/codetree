665c7c0152b5f04543a3e88a312e9b55986a53ed
junheon2005 <junheon2005@g.skku.edu>
2026-08-10 21:46:15 +0900

    [Passed] 불안한 무빙워크 (5ms, 0MB)

--- samsung-sw/불안한 무빙워크/unstable-moving-walk.cpp
+++ samsung-sw/불안한 무빙워크/unstable-moving-walk.cpp
@@ -4,40 +4,37 @@ using namespace std;
 
 int n, k;
 vector<int> movwalk;
-vector<int> value;
-vector<bool> charge;
+vector<int> stabvalue;
+vector<bool> stand;
 
 
 bool count(){
     int cnt = 0;
     for(int i = 1; i <= 2 * n; i++){
-        if(value[movwalk[i]] <= 0) cnt++;
+        if(stabvalue[movwalk[i]] == 0) cnt++;
     }
     if(cnt >= k) return false;
     else return true;
 }
 
+//무빙워크 회전
 void rotate(){
     int temp = movwalk[2 * n];
+    for(int i = 2 * n; i > 1; i--) movwalk[i] = movwalk[i - 1];
 
-    for(int i = 2 * n; i > 1; i--){
-        movwalk[i] = movwalk[i - 1];
-    }
     movwalk[1] = temp;
-    if(charge[movwalk[n]] == true) charge[movwalk[n]] = false;
+
+    //만약 n번째 칸 위라면 비움
+    if(stand[movwalk[n]] == true) stand[movwalk[n]] = false;
 }
 
+//한 칸씩 이동
 void move(){
-    if(charge[movwalk[n - 1]] == true && value[movwalk[n]] > 0){
-        value[movwalk[n]]--;
-        charge[movwalk[n - 1]] = false;
-    }
-    for(int i = n - 2; i >= 1; i--){
-        if(charge[movwalk[i]] == true && charge[movwalk[i + 1]] == false && value[movwalk[i + 1]] > 0){
-            charge[movwalk[i + 1]] = true;
-            charge[movwalk[i]] = false;
-            value[movwalk[i + 1]]--;
-
+    for (int i = n - 1; i >= 1; i--) {
+        if (stand[movwalk[i]] && !stand[movwalk[i + 1]] && stabvalue[movwalk[i + 1]] > 0) {
+            stabvalue[movwalk[i + 1]]--;
+            stand[movwalk[i]] = false;
+            if (i + 1 != n) stand[movwalk[i + 1]] = true;  //만약 n번째 칸 위라면 비움
         }
     }
 }
@@ -46,21 +43,21 @@ int main() {
     cin >> n >> k;
 
     movwalk.resize(2 * n + 1);
-    charge.resize(2 * n + 1, false);
-    value.resize(2 * n + 1);
+    stand.resize(2 * n + 1, false);
+    stabvalue.resize(2 * n + 1);
 
     for(int i = 1; i <= 2 * n; i++){
         movwalk[i] = i;
-        cin >> value[i];
+        cin >> stabvalue[i];
     }
     int turn = 0;
 
     while(count()){
         rotate();
         move();
-        if(!charge[movwalk[1]] && value[movwalk[1]] > 0) {
-            charge[movwalk[1]] = true;
-            value[movwalk[1]]--;
+        if(!stand[movwalk[1]] && stabvalue[movwalk[1]] > 0) {
+            stand[movwalk[1]] = true;
+            stabvalue[movwalk[1]]--;
         }
         turn++;
         /*
