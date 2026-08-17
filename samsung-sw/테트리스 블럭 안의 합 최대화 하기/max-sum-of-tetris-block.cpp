c293a8df7a6f447614a9d5759bd03a1f2ff7ec0c
junheon2005 <junheon2005@g.skku.edu>
2026-08-17 16:46:46 +0900

    [Passed] 테트리스 블럭 안의 합 최대화 하기 (4ms, 0MB)

---
+++ samsung-sw/테트리스 블럭 안의 합 최대화 하기/max-sum-of-tetris-block.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n, m, cur_max = 0;
+int dx[4] = {1, -1, 0, 0};
+int dy[4] = {0, 0, 1, -1};
+vector<vector<int>> grid;
+vector<vector<bool>> visited;
+vector<pair<int, int>> selected;
+
+
+void find_max(int count){
+    if (count == 4) {
+        int res = 0;
+        for (auto [a, b] : selected) res = res + grid[a][b];
+        cur_max = max(cur_max, res);
+        return;
+    }
+
+    for (auto [x, y] : selected) {
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx >= 0 && ny >= 0 && nx < n && ny < m && visited[nx][ny] == false) {
+                selected.push_back({nx, ny});
+                visited[nx][ny] = true;
+                find_max(count + 1);
+                selected.pop_back();
+                visited[nx][ny] = false;
+            }
+        }
+    }
+
+}
+
+
+int main() {
+    // Please write your code here.
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m;
+    grid.resize(n, vector<int>(m));
+    visited.resize(n, vector<bool>(m, false));
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            cin >> grid[i][j];
+        }
+    }
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            visited[i][j] = true;
+            selected.push_back({i, j});
+            find_max(1);
+            selected.pop_back();
+            visited[i][j] = false;
+        }
+    }
+    cout << cur_max;
+    return 0;
+}
